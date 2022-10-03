de045f20b91bad7afe7021d4940cc760d7d2af7e
Kushal <kushaltewari@cedcoss.com>
2022-10-03 16:15:43 +0530

    useMemo Task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useMemo, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const displayRef = useRef(null);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [buttonState, setButtonState] = useState(false);
+
+  const calculate = () => {
+    if(num1 === "" || num2 === "")
+    {
+      return;
+    }
+    else
+    {
+      displayRef.current.innerHTML = "Sum = " +(Number(num1)+Number(num2));
+    } 
+  }
+
+  const multiply = useMemo(() => {
+    if(num1 === "" || num2 === "")
+    {
+      return;
+    }
+    console.log((Number(num1)*Number(num2)));
+
+  }, [num1, num2])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {multiply}
+      <div className="row">
+      <input type="number" onChange={e => setNum1(e.target.value)}/>
+      <input type="number" onChange={e => setNum2(e.target.value)}/>
+      <button onClick={calculate}>Calculate</button>
+      </div>
+      {(num1==="" || num2==="") ? null : <p ref={displayRef}></p>}
+      <button style={{backgroundColor: buttonState? 'dodgerblue' : 'crimson'}} onClick={()=>{setButtonState(!buttonState)}}>Change Button Color</button>
     </div>
   );
 }
